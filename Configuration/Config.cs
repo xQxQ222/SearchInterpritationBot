1bf41aef68a3c9efb68953667c01dae5530d2acc
Ivan Korolkov <nerounxqxq@gmail.com>
2024-10-26 00:22:15 +0400

    Добавлены классы для конфигурации проекта

---
+++ Configuration/Config.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using SearchInterpritationBot.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchInterpritationBot.Config
+{
+    public class Config
+    {
+        public static SettingsStorage BotSettings { get; private set; } = new SettingsStorage();
+        public static void SetProperties(IConfiguration configuration)
+        {
+            BotSettings = GetSection<SettingsStorage>(configuration, "StorageTgBotSets");
+        }
+        private static T GetSection<T>(IConfiguration configuration, string sectionName)
+        {
+            return configuration.GetSection(sectionName).Get<T>()
+                ?? throw new InvalidOperationException($"Not found section {nameof(T)} in configuration.");
+        }
+    }
+}
---
+++ Configuration/SettingsStorage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchInterpritationBot.Config
+{
+    public class SettingsStorage
+    {
+        public string BotTocken { get; set; } = string.Empty;
+    }
+}
